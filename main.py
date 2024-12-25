2f1b2000df1d4c0cf8296e4d8b5a883594017496
xannyx <xander@xvproductions.com>
2024-12-25 18:35:28 +0100

    Added day4

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from typing import Tuple
 from day1.day1 import day1
 from day2.day2 import day2
 from day3.day3 import day3
+from day4.day4 import day4
 from utils.read_file import read_file
 
-# from day4.day4 import day4
 # from day5.day5 import day5
 
 
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     lines = read_file("day3/input.txt")
     print_solution("Day 3:", day3(lines))
 
-    # lines = read_file("day4/input.txt")
-    # print_solution("Day 4:", day4(lines))
-    #
+    lines = read_file("day4/input.txt")
+    print_solution("Day 4:", day4(lines))
+
     # lines = read_file("day5/input.txt")
     # print_solution("Day 5:", day5(lines))
